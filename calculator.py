2eedd46b0d1a58683c88e7eb650d8b75abe91539
smriti chhetry <chhetrysmriti@gmail.com>
2026-07-25 08:47:34 +0545

    Basic Calculatur

--- calculator.py
+++ calculator.py
@@ -1,1 +1,16 @@
-a=int("enter number")
+a=float(input("Enter first number = "))
+b=float(input("Enter second number = "))
+c=input("Choose operator like (+,-,*,/) = ")
+if c=="+":
+    print("Sum=",a+b)
+elif c=="-":
+    print("Difference =",a-b)
+elif c=="*":
+    print("Product =",a*b);
+elif c=="/":
+    if b!=0:
+        print("Result =",a/b)
+    else:
+        print("ERROR!!! Division with zero isnot possible")
+else:
+    print("Invalid operator")
