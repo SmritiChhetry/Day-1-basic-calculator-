9fbee7b18c01b6cc9e3c944bb1a19b730357e126
smriti chhetry <chhetrysmriti@gmail.com>
2026-07-29 19:00:43 +0545

    updated

--- guess.py
+++ guess.py
@@ -1,7 +1,7 @@
 import random
-secret_number = random.randint(1,100)
+secret_number = random.randint(1,200)
 while True:
-    guess=int(input(" 🤔Guess a number between 1 to 100 : "))
+    guess=int(input(" 🤔Guess a number between 1 to 200 : "))
     if guess>secret_number:
         print("Smaller ⬇")
     elif guess<secret_number:
