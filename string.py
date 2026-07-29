df4a102b18997ec9d05e7fcf8d226e5edffc35c3
smriti chhetry <chhetrysmriti@gmail.com>
2026-07-29 19:05:58 +0545

    Remove incomplete string.py

--- string.py
+++
@@ -1,5 +0,0 @@
-#practicing some questions of strings like(counting vowels,reversing strings, checking palindrome)
-print("for counting vowels and consonents")
-a=input("Enter any string: ")
-count=0
-while 
